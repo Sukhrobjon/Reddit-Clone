f2db4f6b6eb694ad7ecd72eb006edd3ecd880175
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-12-03 15:05:14 -0800

    sign-up routehalf-way done

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,27 +1,51 @@
-const Post = require('../models/post');
-
-module.exports = app => {
+const Post = require('../models/post.js');
+const User = require('../models/user.js')
+module.exports = function(app) {
     
     // INDEX
     app.get('/', (req, res) => {
-        res.render('home', { msg: 'Handlebars are Cool!'})
-    })
+        Post.find()
+            .then(post => {
+                res.render('posts-index', { post: post });
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    });
     // NEW
     app.get('/posts/new', (req, res) => {
-        res.render('posts-new')
+        res.render('posts-new', {})
     })
     
-     // CREATE
-     app.post('/posts/new', (req, res) => {
-         // INSTANTIATE INSTANCE OF POST MODEL
-         const post = new Post(req.body);
-
-         // SAVE INSTANCE OF POST MODEL TO DB
-         post.save((err, post) => {
-             // REDIRECT TO THE ROOT
-             console.log(req.body)
-             return res.redirect(`/`);
+    // CREATE
+    app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+        const post = new Post(req.body);
+        
+        // SAVE INSTANCE OF POST MODEL TO DB
+        post.save((err, post) => {
+            // REDIRECT TO THE ROOT
+            return res.redirect(`/`);
             
-         })
-     })
+        })
+    })
+
+    // SIGN UP FORM
+    app.get("/sign-up", (req, res) => {
+        res.render("sign-up");
+    });
+
+    app.post("/sign-up", (req, res) => {
+        // Create User
+        const user = new User(req.body);
+        user
+            .save()
+            .then(user => {
+              res.redirect("/");
+            })
+            .catch(err => {
+              console.log(err.message);
+            });
+    });
+
 };
--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 const expressValidator = require('express-validator')
+
 const port = process.env.PORT || 3000 
-
-// Set db
-require('./data/reddit-db');
-// controller
-require('./controllers/posts.js')(app);
-
 const bodyParser = require('body-parser');
+// controllers
+const posts = require('./controllers/posts.js')
+// Set db
+const database = require('./data/reddit-db');
 
+//mongodb
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reddit-clone", {useNewUrlParser: true});
 
 // Handlebars 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -21,7 +23,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator()); 
 
+// linking the files
+
 // PORT
 app.listen(port, () => {
     console.log('App listening on port 3000!')
 })
+
+posts(app)
+module.exports = app;
